591681c39f73e13181e759c805b2a1bd063a77f6
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-05-25 23:13:08 +0300

    Initial Commit

---
+++ Contacts_SwiftUI/Contacts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_SwiftUIApp.swift
+//  Contacts_SwiftUI
+//
+//  Created by Евгения Шевцова on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
